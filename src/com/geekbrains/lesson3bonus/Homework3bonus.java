b28b38dc8efd9e2e6199972a73c4621f82c047ca
limescode <katechizis@gmail.com>
2022-05-18 14:12:51 +0200

    Оптимизация кода

--- src/com/geekbrains/lesson3bonus/Homework3bonus.java
+++ src/com/geekbrains/lesson3bonus/Homework3bonus.java
@@ -3,36 +3,31 @@ package com.geekbrains.lesson3bonus;
 import java.util.Arrays;
 
 public class Homework3bonus {
+
     public static void main(String[] args) {
-        arrayTwoDimensional1();
+        int[][] arr = new int[4][4]; //сам массив лучше генерировать вне методов - лучше передавать его в методы как параметр
+        arrayTwoDimensional1(arr);
         System.out.println();
-        arrayTwoDimensional2();
+        arrayTwoDimensional2(arr);
     }
-    public static int[][] arrayTwoDimensional1() {
-        int[][] arr = new int[4][4];
+
+    public static int[][] arrayTwoDimensional1(int[][] arr) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++)
-            if (arr[i] == arr[j]) {
-                arr[i][j] = 1;
-            }
+                if (i == j) { //в данном случае в условии надо проверять на равенство не элементы массива, а индексы
+                    arr[i][j] = 1;
+                }
             System.out.println(Arrays.toString(arr[i]));
         }
         return arr;
     }
-    public static int[][] arrayTwoDimensional2() {
-        int[][] arr = new int[4][4];
+
+    public static int[][] arrayTwoDimensional2(int[][] arr) {
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++){
-                if (i == 0 & j == 3) {
-                    arr[i][j] = 1;
-                }
-                if (i == 1 & j == 2) {
-                    arr[i][j] = 1;
-                }
-                if (i == 2 & j == 1) {
-                    arr[i][j] = 1;
-                }
-                if (i == 3 & j == 0) {
+            for (int j = 0; j < arr[i].length; j++) {
+
+                //все проверки на заполнение второй диагонали можно заменить вот таким элегантым условием
+                if (i + j + 1 == arr.length) {
                     arr[i][j] = 1;
                 }
             }
